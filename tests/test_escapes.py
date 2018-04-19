50ebc1b873b8ccb45b8317e87479a7721b8cbeac
Andrew Gross <andrew.w.gross@gmail.com>
2018-04-19 17:48:18 -0400

    Test escaping escapes

--- tests/test_escapes.py
+++ tests/test_escapes.py
@@ -6,3 +6,9 @@ def test_simple_quote_escape():
     path = get_file_path("escape_quote")
     result = read(path, quote='"')
     assert result == [('foo"oo', "bar"), ("baz", "bat")]
+
+
+def test_double_escape():
+    path = get_file_path("escape_escape")
+    result = read(path, quote='"')
+    assert result == [('fo\\o', "bar"), ("baz", "bat")]
